b5b15d284a7aa97864673567c201f73ace233d7e
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2023-12-13 19:05:47 -0300

    new file:   data-base.sql

---
+++ data-base.sql
@@ -0,0 +1,40 @@
+create database thegame;
+
+use thegame;
+
+create table jogador(
+cod_jogador int primary key auto_increment,
+nome varchar(50),
+nickname varchar(50),
+sexo char(1),
+email varchar(50),
+dt_nasc date,
+pontuacao int, 
+moedas int);
+
+create table poder(
+cod_poder int primary key,
+habilidade varchar(50));
+
+create table adquire_poder(
+cod_aquisicao int primary key,
+cod_jogador int,
+cod_poder int,
+dt_hora_aquisicao datetime,
+foreign key(cod_jogador) references jogador (cod_jogador),
+foreign key(cod_poder) references poder (cod_poder));
+
+create table cenario(
+cod_cenario int primary key,
+caracteristicas varchar(50),
+qtde_min_pontos int );
+
+create table partida(
+cod_partida int primary key auto_increment,
+data_hora_inicio datetime,
+data_hora_termino datetime,
+pontos_obtidos int,
+cod_jogador int,
+cod_cenario int,
+foreign key(cod_jogador) references jogador (cod_jogador),
+foreign key(cod_cenario) references cenario (cod_cenario));
