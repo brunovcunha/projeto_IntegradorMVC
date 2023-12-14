f0d6fa9ec56a29766cd2eeba1df18c05fb65266c
João Vi-Vi Neto <133411181+JoaoVitorinoVN@users.noreply.github.com>
2023-12-14 17:16:14 -0300

    Update para arrumar bugs e experimentar

--- src/main/java/com/iftm/projetofinal/app/Controller/GameController.java
+++ src/main/java/com/iftm/projetofinal/app/Controller/GameController.java
@@ -4,21 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.iftm.projetofinal.app.dao.GameDao;
 
 @Controller
 public class GameController {
     
     @Autowired
-    private GameDao dao;
 
-    @GetMapping("")
-    public String index() {
-        return "login";
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView("login");
+        mv.addObject("usuario", null);
+        return mv;
     }
 
-    @GetMapping("index")
+    @GetMapping("/menu")
     public ModelAndView menuPrincipal() {
         ModelAndView mv = new ModelAndView("menu");
         mv.addObject("usuario", null);
--- src/main/java/com/iftm/projetofinal/app/dao/GameDao.java
+++ src/main/java/com/iftm/projetofinal/app/dao/GameDao.java
@@ -1,13 +1,9 @@
 package com.iftm.projetofinal.app.dao;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
-import com.iftm.projetofinal.app.domain.Jogadores;
 
 @Component
 public class GameDao {
@@ -15,11 +11,6 @@ public class GameDao {
     @Autowired
     JdbcTemplate data;
 
-    public List<Jogadores> getJogadores() {
-        String SQL = "select nickname, email from jogador where lower(nickname) like ?";
-
-        return data.query(SQL, new Object[] { "%+ nickname +%" }, new BeanPropertyRowMapper<Jogadores>(Jogadores.class));
-    }
 
     
 }
