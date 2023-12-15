e21c7f3d3125dfb9b5f8351148b7bc537d8b88e9
Bruno Vieira <133446390+brunovcunha@users.noreply.github.com>
2023-12-15 18:15:01 -0300

    Update LoginController.java

--- src/main/java/com/iftm/projetofinal/app/Controller/LoginController.java
+++ src/main/java/com/iftm/projetofinal/app/Controller/LoginController.java
@@ -15,14 +15,13 @@ import org.springframework.security.web.authentication.logout.SecurityContextLog
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.iftm.projetofinal.app.domain.Login;
 import com.iftm.projetofinal.app.domain.Role;
 import com.iftm.projetofinal.app.service.LoginService;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -72,6 +71,19 @@ public class LoginController {
                 return menuController.getMenus(model,loginBanco.getRoles());
     }
 
+     @GetMapping("/login/telaNovoUsuario")
+    public String novoUsuarioForm(Model model) {
+        model.addAttribute("login", new Login());
+        return "cadastro";
+    }
+
+    @PostMapping("/login/novoUsuario")
+    public String salvarLogin(Login login, Model model) {
+        loginService.salvar(login);
+        model.addAttribute("mensagem", "Usuário salvo com sucesso");
+        return "redirect:/";
+    }
+    
     @RequestMapping("/login/sair")
     public String customLogoutMethod(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
@@ -80,4 +92,5 @@ public class LoginController {
         }
         return "login";
     }
+    
 }
