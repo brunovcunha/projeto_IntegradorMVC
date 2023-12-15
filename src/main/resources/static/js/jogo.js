d29c9119b40c295ee19e74bad9704df3ddb0c61d
Vinicius Raphael <133443384+Vini-IFTM@users.noreply.github.com>
2023-12-15 19:37:12 -0300

    Update jogo.js

--- src/main/resources/static/js/jogo.js
+++ src/main/resources/static/js/jogo.js
@@ -1,52 +1,53 @@
-function acao(jogada) {
+window.addEventListener("load", function () {
+    document.body.style.backgroundImage = `url("/img/cenario/castelo.png")`;
+    function acao(jogada) {
+        let jogador1PodeAtacar = true;
+        let jogador2PodeAtacar = true;
+        let jogadaInimigo = Math.floor(Math.random() * 3) + 1;
+        let resultadoTexto = document.getElementById("texto");
 
-    let jogador1PodeAtacar = true;
-    let jogador2PodeAtacar = true;
+        let imgJogador1 = document.getElementById("imgJogador1");
+        let imgJogador2 = document.getElementById("imgJogador2");
 
-    let jogadaInimigo = Math.floor(Math.random() * 3) + 1;
-    let resultadoTexto = document.getElementById("texto");
+        if (jogada === 1 && jogadaInimigo === 3) {
+            resultadoTexto.innerHTML = "Vitória";
 
-    let imgJogador1 = document.getElementById("imgJogador1");
-    let imgJogador2 = document.getElementById("imgJogador2");
+            imgJogador1.src = "/img/cavaleiro_ataque.png";
+            imgJogador2.src = "/img/cavaleiro_descansa.png";
+        } else if (jogada === 3 && jogadaInimigo === 1) {
+            resultadoTexto.innerHTML = "Derrota";
 
-    if (jogada === 1 && jogadaInimigo === 3) {
-        resultadoTexto.innerHTML = "Vitória";
+            imgJogador1.src = "/img/cavaleiro_descansa.png";
+            imgJogador2.src = "/img/cavaleiro_ataque.png";
 
-        imgJogador1.src = "../static/img/cavaleiro_ataque.png";
-        imgJogador2.src = "../static/img/cavaleiro_descansa.png";
-    } else if (jogada === 3 && jogadaInimigo === 1) {
-        resultadoTexto.innerHTML = "Derrota";
+        } else if (jogada === 1 && jogadaInimigo === 2) {
+            resultadoTexto.innerHTML = "Defendeu";
 
-        imgJogador1.src = "../static/img/cavaleiro_descansa.png";
-        imgJogador2.src = "../static/img/cavaleiro_ataque.png";
+            imgJogador1.src = "/img/cavaleiro_ataque.png";
+            imgJogador2.src = "/img/cavaleiro_defesa.png";
+        }
+        else if (jogada === 2 && jogadaInimigo === 1) {
+            resultadoTexto.innerHTML = "Defendeu";
 
-    } else if (jogada === 1 && jogadaInimigo === 2){
-        resultadoTexto.innerHTML = "Defendeu";
+            imgJogador1.src = "/img/cavaleiro_defesa.png";
+            imgJogador2.src = "/img/cavaleiro_ataque.png";
+        }
+        else if (jogada === 1 && jogadaInimigo === 1) {
+            resultadoTexto.innerHTML = "Empate";
 
-        imgJogador1.src = "../static/img/cavaleiro_ataque.png";
-        imgJogador2.src = "../static/img/cavaleiro_defesa.png";
+            imgJogador1.src = "/img/cavaleiro_ataque.png";
+            imgJogador2.src = "/img/cavaleiro_ataque.png";
+
+        } else if (jogada === 2 && jogadaInimigo === 2) {
+            resultadoTexto.innerHTML = "Nada Aconteceu";
+
+            imgJogador1.src = "/img/cavaleiro_defesa.png";
+            imgJogador2.src = "/img/cavaleiro_defesa.png";
+        } else if (jogada === 3 && jogadaInimigo === 3) {
+            resultadoTexto.innerHTML = "Nada Aconteceu";
+
+            imgJogador1.src = "/img/cavaleiro_descansa.png";
+            imgJogador2.src = "/img/cavaleiro_descansa.png";
+        }
     }
-    else if (jogada === 2 && jogadaInimigo === 1){
-        resultadoTexto.innerHTML = "Defendeu";
-
-        imgJogador1.src = "../static/img/cavaleiro_defesa.png";
-        imgJogador2.src = "../static/img/cavaleiro_ataque.png";
-    }
-    else if (jogada === 1 && jogadaInimigo === 1) {
-        resultadoTexto.innerHTML = "Empate";
-
-        imgJogador1.src = "../static/img/cavaleiro_ataque.png";
-        imgJogador2.src = "../static/img/cavaleiro_ataque.png";
-
-    } else if (jogada === 2 && jogadaInimigo === 2) {
-        resultadoTexto.innerHTML = "Nada Aconteceu";
-
-        imgJogador1.src = "../static/img/cavaleiro_defesa.png";
-        imgJogador2.src = "../static/img/cavaleiro_defesa.png";
-    } else if (jogada === 3 && jogadaInimigo === 3) {
-        resultadoTexto.innerHTML = "Nada Aconteceu";
-
-        imgJogador1.src = "../static/img/cavaleiro_descansa.png";
-        imgJogador2.src = "../static/img/cavaleiro_descansa.png";
-    }
-}
+});
